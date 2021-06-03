0eae2f4460331dd79f3ecabcb5f31177f89ccc24
RocketBuilder <84983353+RocketBuilder@users.noreply.github.com>
2021-06-03 16:24:28 +0200

    Update Test20.py

--- Test20.py
+++ Test20.py
@@ -1,31 +1,48 @@
+# Import libraries
 import RPi.GPIO as GPIO
 import time
 
-servoPIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+# Set GPIO numbering mode
+GPIO.setmode(GPIO.BOARD)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
-p.start(2.5) # Initialisierung
-try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
- 
+# Set pin 11 as an output, and set servo1 as pin 11 as PWM
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50) # Note 11 is pin, 50 = 50Hz pulse
+
+#start PWM running, but with value of 0 (pulse off)
+servo1.start(0)
+print ("Waiting for 2 seconds")
+time.sleep(2)
+
+#Let's move the servo!
+print ("Rotating 180 degrees in 10 steps")
+
+# Define variable duty
+duty = 2
+
+# Loop for duty values from 2 to 12 (0 to 180 degrees)
+while duty <= 12:
+    servo1.ChangeDutyCycle(duty)
+    time.sleep(1)
+    duty = duty + 1
+
+# Wait a couple of seconds
+time.sleep(2)
+
+# Turn back to 90 degrees
+print ("Turning back to 90 degrees for 2 seconds")
+servo1.ChangeDutyCycle(7)
+time.sleep(2)
+
+#turn back to 0 degrees
+print ("Turning back to 0 degrees")
+servo1.ChangeDutyCycle(2)
+time.sleep(0.5)
+servo1.ChangeDutyCycle(0)
+
+#Clean things up at the end
+servo1.stop()
+GPIO.cleanup()
+print ("Goodbye")
+
+
